f4b3ffd947d45c2071b28f2b42a59463397c49ff
adv003 <110819522+adv003@users.noreply.github.com>
2024-04-30 02:32:22 +0530

    Update register.php

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="csssss.css">
+    <link rel="stylesheet" href="css\csssss.css">
     <title>Register</title>
 </head>
 <body>
@@ -72,7 +72,7 @@
                     <input type="submit" class="btn" name="submit" value="Register" required>
                 </div>
                 <div class="links">
-                    Already a member? <a href="ind.php">Sign In</a>
+                    Already a member? <a href="cookie.php">Sign In</a>
                 </div>
             </form>
         </div>
@@ -83,16 +83,7 @@
     <link rel = "stylesheet" href="style.css">
 </head>
 <body>
-<div id="cookies">
-    <div class="container">
-        <div class="subcontainer"> 
-            <div class="cookies">
-                <p> This website uses cookies.<a href=""> More info.</a> </p>
-                <button id="cookies-btn">ok</button>
-            </div>
-        </div>    
-    </div>
-</div>
+
 <script src="java.js"></script>
 </body>       
 
